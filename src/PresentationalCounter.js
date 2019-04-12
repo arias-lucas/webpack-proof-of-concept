0df83b6cfda7226309b3821df9d5d575697414d6
Lucas Arias <>
2019-04-12 10:05:50 -0300

    Added Material UI library.

--- src/PresentationalCounter.js
+++ src/PresentationalCounter.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
 
 const PresentationalCounter = React.memo(props => {
   return (
     <div style={{ textAlign: "center" }}>
-      {props.counter}
-      <br />
-      <button onClick={props.incrementCounter}>Increment</button>
+      <div>{props.counter}</div>
+      <Button variant="outlined" onClick={props.incrementCounter}>
+        Increment
+      </Button>
     </div>
   );
 });
